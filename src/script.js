6c2ac2a44541466f75e8ee459b4e14c5781c989a
dafsuy <jangkrikuburubur@gmail.com>
2023-09-05 21:50:36 +0700

    first

---
+++ src/script.js
@@ -0,0 +1,50 @@
+//Navber Fixed
+window.onscroll = () => {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+  setTimeout(() => {
+    navMenu.classList.toggle('opacity-0');
+  }, 100);
+});
+
+// animate show
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  const elements = document.querySelectorAll('.animate-up');
+  elements.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add('show');
+    }
+  });
+}
+
+// Tambahkan event listener scroll
+window.addEventListener('scroll', handleScroll);
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2c632c',
+        secondary: '#08be6a',
+        accent: '#e26c2d'
+      },
+      fontFamily: {
+        inter: ['Inter'],
+        sono: ['Sono']
+      },
+      keyframes: {
+        'slide-right': {
+          '0%': { opacity: '0', transform: 'translateX(-4rem)' },
+          '100%': { opacity: '100', transform: 'translateX(0)' }
+        },
+        'slide-left': {
+          '0%': { opacity: '0', transform: 'translateX(4rem)' },
+          '100%': { opacity: '100', transform: 'translateX(0)' }
+        },
+        'slide-up': {
+          '0%': { opacity: '0', transform: 'translateY(4rem)' },
+          '100%': { opacity: '100', transform: 'translateY(0)' }
+        },
+        'slide-down': {
+          '0%': { opacity: '0', transform: 'translateY(-4rem)' },
+          '100%': { opacity: '100', transform: 'translateY(0)' }
+        },
+        'float': {
+          '0%': {transform: 'translateY(0px)'},
+          '50%': {transform: 'translateY(20px)'},
+          '100%': {transform: 'translateY(0px)'},
+        },
+      },
+      animation: {
+        'slide-right': 'slide-right 1s ease-out',
+        'slide-left': 'slide-left 1s ease-out',
+        'slide-up': 'slide-up 1s ease-out',
+        'slide-down': 'slide-down 1s ease-out',
+        'spin-slow': 'spin 60s linear infinite',
+        'float':  'float 6s ease-in-out infinite',
+      }
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#2c632c',
+
+          secondary: '#08be6a',
+
+          accent: '#e26c2d',
+
+          neutral: '#181b25',
+
+          'base-100': '#eae2ee',
+
+          info: '#9bd6e4',
+
+          success: '#1c8757',
+
+          warning: '#c69606',
+
+          error: '#ee5749'
+        }
+      }
+    ]
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')]
+};
+// #e26c2d green old
+// #2c632c orange
+// #08be6a green young
